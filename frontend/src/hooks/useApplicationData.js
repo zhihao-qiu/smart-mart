676a8fefdf9a4cb61a0512a830288e6724950e2a
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-07-11 16:52:06 -0700

    Regular: implement frontend

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -26,7 +26,7 @@ export default function useApplicationData() {
     // }
     const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
     const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
-    const ALGOLIA_INDEX_NAME = ALGOLIA_INDEX_NAME;
+    const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME;
 
 
     const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
