9c247960ba32283a7a3b6cde6d41e391b9f499be
Bastien Moy <moy.bastien@gmail.com>
2023-07-11 16:26:21 -0700

    adding initial query for all products

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,16 +1,16 @@
-// const pg = require("pg");
+const pg = require("pg");
 
-// const client = new pg.Client({
-//   host: process.env.DB_HOST,
-//   name: process.env.DB_NAME,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   port: process.env.DB_PORT,
-//   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
-// });
+const client = new pg.Client({
+  host: process.env.DB_HOST,
+  name: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+});
 
-// client
-//   .connect()
-//   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+client
+  .connect()
+  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
 
-// module.exports = client;
+module.exports = client;
