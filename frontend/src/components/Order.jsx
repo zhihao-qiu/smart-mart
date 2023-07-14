9f368920634402359e45b69b273aa3a94cf6238e
Bastien Moy <moy.bastien@gmail.com>
2023-07-14 12:07:02 -0700

    editing order details page

--- frontend/src/components/Order.jsx
+++ frontend/src/components/Order.jsx
@@ -3,11 +3,9 @@ import React from 'react';
 const Order = (props) => {
 
   const cart = props.Cart;
-  console.log(props);
+
   const orderList = cart.map((item) => {
     return (
-      <div className="flex flex-col justify-start items-start bg-gray-50 px-4 py-4 md:py-6 md:p-6 xl:p-8 w-full">
-                    <p className="text-lg md:text-xl font-semibold leading-6 xl:leading-5 text-gray-800">Order Details</p>
                     <div className="mt-4 md:mt-6 flex  flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full ">
                         <div className="pb-4 md:pb-8 w-full md:w-40">
                             <img className="w-full hidden md:block" src={item.image} alt="dress" />
@@ -29,64 +27,49 @@ const Order = (props) => {
                             </div>
                         </div>
                     </div>
-                </div>
     )
   })
 
-
+  const subTotal = cart.reduce((currentTotal, item) => currentTotal + item.price, 0);
+  const gst = (subTotal * 0.05).toFixed(2);
+  const pst = (subTotal * 0.07).toFixed(2);
+  const total = (parseInt(subTotal) + parseInt(gst) + parseInt(pst)).toFixed(2);
 
   return (
     <div className="py-14 px-4 md:px-6 2xl:px-20 2xl:container 2xl:mx-auto">
         <div className="flex justify-start item-start space-y-2 flex-col ">
             <h1 className="text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9  text-gray-800">Order #13432</h1>
-            <p className="text-base font-medium leading-6 text-gray-600">21st Mart 2021 at 10:34 PM</p>
         </div>
         <div className="mt-10 flex flex-col xl:flex-row jusitfy-center items-stretch  w-full xl:space-x-8 space-y-4 md:space-y-6 xl:space-y-0">
             <div className="flex flex-col justify-start items-start w-full space-y-4 md:space-y-6 xl:space-y-8">
-              {orderList}
-                <div className="flex justify-center md:flex-row flex-col items-stretch w-full space-y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
+                  <div className="flex flex-col justify-start items-start bg-gray-50 px-4 py-4 md:py-6 md:p-6 xl:p-8 w-full">
+                    <p className="text-lg md:text-xl font-semibold leading-6 xl:leading-5 text-gray-800">Order Details</p>
+                  {orderList}
+                  </div>
+                <div className="flex justify-center md:flex-row flex-col items-stretch w-2/4 y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
                     <div className="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 space-y-6   ">
-                        <h3 className="text-xl font-semibold leading-5 text-gray-800">Summary</h3>
+                        <h3 className="text-xl font-semibold leading-5 text-gray-800">Total</h3>
                         <div className="flex justify-center items-center w-full space-y-4 flex-col border-gray-200 border-b pb-4">
                             <div className="flex justify-between  w-full">
                                 <p className="text-base leading-4 text-gray-800">Subtotal</p>
-                                <p className="text-base leading-4 text-gray-600">$56.00</p>
+                                <p className="text-base leading-4 text-gray-600">${subTotal}</p>
                             </div>
                             <div className="flex justify-between items-center w-full">
-                                <p className="text-base leading-4 text-gray-800">
-                                    Discount <span className="bg-gray-200 p-1 text-xs font-medium leading-3  text-gray-800">STUDENT</span>
-                                </p>
-                                <p className="text-base leading-4 text-gray-600">-$28.00 (50%)</p>
+                                <p className="text-base leading-4 text-gray-800">GST/HST</p>
+                                <p className="text-base leading-4 text-gray-600">${gst}</p>
+                            </div>
+                            <div className="flex justify-between items-center w-full">
+                                <p className="text-base leading-4 text-gray-800">PST</p>
+                                <p className="text-base leading-4 text-gray-600">${pst}</p>
                             </div>
                             <div className="flex justify-between items-center w-full">
                                 <p className="text-base leading-4 text-gray-800">Shipping</p>
-                                <p className="text-base leading-4 text-gray-600">$8.00</p>
+                                <p className="text-base leading-4 text-gray-600">FREE</p>
                             </div>
                         </div>
                         <div className="flex justify-between items-center w-full">
                             <p className="text-base font-semibold leading-4 text-gray-800">Total</p>
-                            <p className="text-base font-semibold leading-4 text-gray-600">$36.00</p>
-                        </div>
-                    </div>
-                    <div className="flex flex-col justify-center px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 space-y-6   ">
-                        <h3 className="text-xl font-semibold leading-5 text-gray-800">Shipping</h3>
-                        <div className="flex justify-between items-start w-full">
-                            <div className="flex justify-center items-center space-x-4">
-                                <div className="w-8 h-8">
-                                    <img className="w-full h-full" alt="logo" src="https://i.ibb.co/L8KSdNQ/image-3.png" />
-                                </div>
-                                <div className="flex flex-col justify-start items-center">
-                                    <p className="text-lg leading-6 font-semibold text-gray-800">
-                                        DPD Delivery
-                                        <br />
-                                        <span className="font-normal">Delivery with 24 Hours</span>
-                                    </p>
-                                </div>
-                            </div>
-                            <p className="text-lg font-semibold leading-6 text-gray-800">$8.00</p>
-                        </div>
-                        <div className="w-full flex justify-center items-center">
-                            <button className="hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 py-5 w-96 md:w-full bg-gray-800 text-base font-medium leading-4 text-white">View Carrier Details</button>
+                            <p className="text-base font-semibold leading-4 text-gray-600">${total}</p>
                         </div>
                     </div>
                 </div>
